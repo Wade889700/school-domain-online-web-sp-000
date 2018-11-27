43ac25c424ffc69c9abbf52750664061a4d8b91f
Hsu-Ting Kao <severus889700@gmail.com>
2018-11-27 07:03:23 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -16,7 +16,7 @@ class School
 
   def add_student(name,number)
     roster[number]||=[] #“”||=“”符號說明：回傳||左邊的值，但假如左邊回傳nil或false，改成把右邊的值指定給左邊，並回傳。
-    roster[number] << name
+    roster[number].push(name)
   end
 
 end
